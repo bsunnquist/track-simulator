c4bd09088fa46dd7757f7e7365f56ac6d72c061d
Jennifer Medina <jennifermedina@Jennifers-MacBook-Pro.local>
2019-05-17 23:52:17 -0400

    made a wrapper + users can set their tournament distance, and adjust the speed, but the relative player velicities are still the same, i'm awesome

--- race_simulation.py
+++ race_simulation.py
@@ -2,77 +2,99 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-#~~~~~~Future Input Parameters~~~~~~
-distance = 400 # meters
-time     = 60  # seconds
-nplayers = 20  # players
+def trackSim(name, distance, time, nplayers, interval):
+    """ Runs the track simulator!
 
-#~~~~~~Bounds of the racetrack~~~~~~
-xlim = (0,distance+(0.5*distance))
-ylim = (0,nplayers)
+    Parameters
+    ----------
+    name : str
+        Your name (or whichever name you want your player to have).
+    distance : int
+        The length you want your player to run for (meters).
+    time : int
+        Your average time for the selected distance (seconds).
+    nplayers : int
+        The amount of runners you want to compete against.
+    interval : int (default = 1)
+        The time (in milliseconds) in between frames. Essentially, this
+        parameter adjusts how long you want the race simulation to run.
+        The lower this number, the shorter the duration. To run the simulation
+        in real-time, the input value should be 1000.
 
-#~~~~~~Figure Aesthetics~~~~~~
-fig = plt.figure()
-ax = fig.add_subplot(111, autoscale_on=False, xlim=xlim, ylim=ylim)
-plt.axvline(distance, color='k')
-plt.text(distance+1, 12, 'Finish!', fontsize=20, rotation=90)
+    Returns
+    -------
+    A matplotlib animation with the race simulation.
+    """
+    #~~~~~~Bounds of the racetrack~~~~~~
+    xlim = (0,distance+(0.5*distance))
+    ylim = (0,nplayers)
 
-class Ball(object):
+    #~~~~~~Figure Aesthetics~~~~~~
+    fig = plt.figure()
+    ax = fig.add_subplot(111, autoscale_on=False, xlim=xlim, ylim=ylim)
+    plt.axvline(distance, color='k')
+    plt.text(distance+1, 12, 'Finish!', fontsize=20, rotation=90)
 
-    def __init__(self, name, x, y, v):
-        """
-        :param x y: Initial position.
-        :param v: Initial velocity.
-        """
-        self.v = float(v)
-        self.x = float(x)
-        self.y = float(y)
-        self.name = name
+    class Ball(object):
 
-        self.scatter, = ax.plot([], [], 'o', markersize=20)
+        def __init__(self, name, x, y, v):
+            """
+            :param x y: Initial position.
+            :param v: Initial velocity.
+            """
+            self.v = float(v)
+            self.x = float(x)
+            self.y = float(y)
+            self.name = name
 
-    def update(self):
-        self.x += self.v
-        self.scatter.set_data(self.x, self.y)
-        if self.x >= distance:
-            if self.name not in rankings:
-                print(self.name)
-                rankings.append(self.name)
-            self.scatter.set_data(distance, self.y)
+            self.scatter, = ax.plot([], [], 'o', markersize=20)
 
-    def start(self):
-        self.scatter.set_data(self.x, self.y)
+        def update(self):
+            self.x += self.v
+            self.scatter.set_data(self.x, self.y)
+            if self.x >= distance:
+                if self.name not in rankings:
+                    print(self.name)
+                    rankings.append(self.name)
+                self.scatter.set_data(distance, self.y)
 
-def init():
-    return []
+        def start(self):
+            self.scatter.set_data(self.x, self.y)
 
-def animate(t):
+    def init():
+        return []
 
-    if t==0.0:
-        print('starting')
-        for ball in balls:
-            ball.start()
-    else:
-        for ball in balls:
-            ball.update()
-            if len(rankings)==len(balls):
-                ani.event_source.stop()
-                print(rankings)
+    def animate(t):
 
-    # have to return an iterable
-    return [ball.scatter for ball in balls]
+        if t==0.0:
+            print('starting')
+            for ball in balls:
+                ball.start()
+        else:
+            for ball in balls:
+                ball.update()
+                if len(rankings)==len(balls):
+                    ani.event_source.stop()
+                    print(rankings)
 
-# calculating v
-v = distance/time
-#                           this # tells you by how much X is going to increase every frame
-#                           example: .3 means X is going up by .3 every 1000ms (which is the interval)
-#
-balls = [Ball('bob', 0, 2, .3),
-         Ball('jenny', 0, 4, v),
-         Ball('ben', 0, 6, .5)]
+        # have to return an iterable
+        return [ball.scatter for ball in balls]
 
-# interval in milliseconds
-# we're watching in slow motion (delta t is shorter than interval)
-rankings = []
-ani = animation.FuncAnimation(fig, animate, np.arange(0, distance, .01), init_func=init, interval=1, blit=True, repeat=False)
-plt.show()
+    frames = np.arange(0, distance, .001)
+
+    balls = []
+    for n in np.arange(0, nplayers, 1):
+        ball = Ball('player '+str(n), 0, 0+n, .3)
+        balls.append(ball)
+
+    player_name = Ball(name, 0, 4, distance/time)
+
+    balls.append(player_name)
+
+
+    # interval in milliseconds
+    # we're watching in slow motion (delta t is shorter than interval)
+    rankings = []
+    ani = animation.FuncAnimation(fig, animate, frames, init_func=init,
+                                  interval=interval, blit=True, repeat=False)
+    plt.show()
